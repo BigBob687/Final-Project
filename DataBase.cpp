e6bee5d31840b0da9d52b3a4fd9a26a0f3b1cdc8
GGBappo <hassanjannan1234@gmail.com>
2025-05-06 23:57:37 -0400

    This is the version that can store all the data and send out all the details and make a patient

---
+++ DataBase.cpp
@@ -0,0 +1,30 @@
+//
+// Created by hassa on 4/28/2025.
+//
+
+#include "DataBase.h"
+#include "Patient_Registry.h"
+#include "System.h"
+using namespace std;
+
+void DataBase::callAllPatients(System& system) {
+
+//Prints out the paitents Vector to see all the paitients inside of it
+    for(auto & Patient : system.Patients) {
+        cout << "Patient Details:\n";
+        cout << "Name: " << Patient.Name << endl;
+        cout << "Age: " << Patient.Age << endl;
+        cout << "Gender: " << Patient.Gender << endl;
+        cout << "Illness: " << Patient.Illness << endl;
+        cout << "Doctor: " <<Patient.DoctorName << endl;
+        cout << "Nurse: " << Patient.NurseName << endl;
+        cout << "Insurance: " << Patient.InsuranceName << endl;
+        cout << "Medication: " << Patient.Medication << endl;
+        cout << "Appointment Info: " << Patient.DT << endl;
+        cout << "Scheduled Appointment: " << Patient.SA << endl;
+        cout << "Date & Time Info: " << Patient.DT << endl;
+        cout << "Follow Up Appointment: " << Patient.FUA << endl;
+        cout << endl;
+        cout << endl;
+    }
+}
---
+++ DataBase.h
@@ -0,0 +1,19 @@
+//
+// Created by hassa on 4/28/2025.
+//
+
+#ifndef DATABASE_H
+#define DATABASE_H
+#include "System.h"
+
+using namespace std;
+
+
+class DataBase {
+    public:
+    void callAllPatients(System& system);
+};
+
+
+
+#endif //DATABASE_H
